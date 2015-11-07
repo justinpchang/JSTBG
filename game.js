ac7628763f8ea7ec5a026675db4bd25a19ec80fa
Justin Chang <justin.p.chang@gmail.com>
2015-11-06 20:33:59 -0500

    Parses input into word array; added dictionary
    Uses .split(“ “) method to split input. Now checks input against
    dictionary for valid input sequence.

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // feed and input setup
   $("#inputBox").bind("enterKey",function(e){
     command = $("#inputBox").val();
     response = respondTo(command);
@@ -11,20 +12,42 @@ $(document).ready(function() {
           $(this).trigger("enterKey");
       }
   });
-
   $("#inputSubmit").click(function() {
-    text = "[" + time + "] " + $("#inputBox").val() + "</p></p>" + text;
+    text = "[" + time + "] " + $("#inputBox").val() + "</p><p>" + text;
     $("#inputBox").val("");
   });
-
   var updateFeed = function() {
     $("#feed").html(text);
   };
-
   setInterval(updateFeed, 100);
 });
 
 function respondTo(command) {
-  // echo back for now
-  return "You typed: " + command;
+  var returnStr = "";
+  var invalid = false;
+
+  // parse command into words
+  input = command.split(" ");
+  for(i = 0; i < input.length; i++) {
+    input[i] = input[i].toLowerCase();
+  }
+
+  // check words against dictionary
+  for(i = 0; i < input.length; i++) {
+    for(j = 0; j < dictionary.length; j++) {
+      if(input[i] == dictionary[j]) {
+        break;
+      }
+      else if(j == dictionary.length - 1 && dictionary.length > 1) {
+        invalid = true;
+        break;
+      }
+    }
+    if(invalid) {
+      return "I do not understand that.";
+      break;
+    }
+  }
+
+  return returnStr;
 }
--- vars.js
+++ vars.js
@@ -1,5 +1,13 @@
+var i,j;
+
 var inputBoxWidth = 100;
 var text = "";
 var time = "";
+
 var command = "";
 var response = "";
+var input = [];
+
+var dictionary = [];
+dictionary.push("hello");
+dictionary.push("potato");
